bfc2ad5ab9661769b980bd3d3240c61ad9edef62
zezomo <zezouniverse7@gmail.com>
2024-03-14 17:24:13 -0700

    zezo

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -5,4 +5,4 @@ name VARCHAR(256),
 score INT
 );
 INSERT INTO second_table (id, name, score)
-VALUES ((1, "John", 10), (2, "Alex", 3), (3, "Bob", 14), (4, "George", 8));
+VALUES (1, "John", 10), (2, "Alex", 3), (3, "Bob", 14), (4, "George", 8);
