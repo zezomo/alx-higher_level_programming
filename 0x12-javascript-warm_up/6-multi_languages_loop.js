ecf0a80a983885697df3118c5a62a86752bcf978
zezomo <zezouniverse7@gmail.com>
2024-04-09 11:25:37 -0700

    push

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,7 +1,5 @@
 #!/usr/bin/node
-
-const langs = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-
-for (let i = 0; i < 3; i++) {
-  console.log(langs[i]);
+const args = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (const arg in args) {
+  console.log(args[arg]);
 }
