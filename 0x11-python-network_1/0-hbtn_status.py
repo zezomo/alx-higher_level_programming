bbd6caf7f7935059eeb6604e0b2a947d42147b55
zezomo <zezouniverse7@gmail.com>
2024-04-26 13:45:29 -0700

    push

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,7 +2,6 @@
 """Fetches https://intranet.hbtn.io/status."""
 import urllib.request
 
-
 if __name__ == "__main__":
     request = urllib.request.Request("https://intranet.hbtn.io/status")
     with urllib.request.urlopen(request) as response:
--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -2,7 +2,6 @@
 """Fetches https://intranet.hbtn.io/status."""
 import requests
 
-
 if __name__ == "__main__":
     r = requests.get("https://intranet.hbtn.io/status")
     print("Body response:")
